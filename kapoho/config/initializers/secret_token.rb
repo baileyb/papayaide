5be738f28c4a0515186cc7763b12f01bbe26af44
Brian Bailey <baileyb@gmail.com>
2012-06-29 14:55:41 -0700

    Created kapoho rails frontend

---
+++ kapoho/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kapoho::Application.config.secret_token = '7f3dd13af6c83ccb8ed100dbca2f324238ec797e77a47df1fcd80d988fea4c09e9c007d9b7e8f33208cb05cba2309f3f71b76f501e132f792b65194d032988c4'
