f02cec6d251933fbe4885746aa375c883ea6d52d
Brian Bailey <baileyb@gmail.com>
2012-06-29 17:49:49 -0700

    Switched to Mongoid, seems like it will scale easier later on

--- kapoho/config/initializers/mongo.rb
+++
@@ -1,8 +0,0 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27107)
-MongoMapper.database = "papayaide-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    MongoMapper.connection.connect if forked
-  end
-end
